2d6c5a960bb639324eb3ad48bf6ba9ad799a73f6
cindy <uenyihung@gmail.com>
2016-12-13 23:12:58 -0500

    first commit

---
+++ src/main/java/com/uenyihung/datastructure/LinkedList/LinkedListNode.java
@@ -0,0 +1,93 @@
+package com.uenyihung.datastructure.LinkedList;
+
+/**
+ * An object that will be used as a node for a LinkList. And object that
+ * contains 3 values. The data value, the previous node address and the next
+ * node address.
+ *
+ * @author Uen Yi Cindy Hung
+ * @since 12/13/2016
+ */
+public class LinkedListNode<T> {
+
+    private T data;
+    private LinkedListNode<T> previous;
+    private LinkedListNode<T> next;
+
+    /**
+     * Default constructor
+     */
+    public LinkedListNode() {
+        data = null;
+        previous = null;
+        next = null;
+    }
+
+    /**
+     * Sets the data value.
+     *
+     * @param data
+     */
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    /**
+     * Sets the node that is previous to @this instance.
+     *
+     * @param node
+     */
+    public void setPrevious(LinkedListNode<T> node) {
+        this.previous = node;
+    }
+
+    /**
+     * Sets the node that is next to @this instance.
+     *
+     * @param node
+     */
+    public void setNext(LinkedListNode<T> node) {
+        this.next = node;
+    }
+
+    /**
+     * Retrieves the data value.
+     *
+     * @return T the data in the node.
+     */
+    public T getData() {
+        return this.data;
+    }
+
+    /**
+     * Retrieves the node that is previous to $this instance.
+     *
+     * @return LinkedListNode<T> the previous node.
+     */
+    public LinkedListNode<T> getPrevious() {
+        return this.previous;
+    }
+
+    /**
+     * Retrieves the node that is next to $this instance.
+     *
+     * @return LinkedListNode the next node.
+     */
+    public LinkedListNode<T> getNext() {
+        return this.next;
+    }
+
+    /**
+     * Returns the String representing the instance of LinkedListNode.
+     * 
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return "LinkedListNode{\n"
+                + "\tValue: " + data + "\n"
+                + "\tPrevious: " + previous + "\n"
+                + "\tNext: " + next + "\n"
+                + "}";
+    }
+}
---
+++ src/main/java/com/uenyihung/datastructure/LinkedList/MyLinkedList.java
@@ -0,0 +1,163 @@
+package com.uenyihung.datastructure.LinkedList;
+
+/**
+ * A generic LinkedList.
+ *
+ * @author Uen Yi Cindy Hung
+ * @since 12/13/2016
+ */
+public class MyLinkedList<T> {
+
+    private int size;
+    private LinkedListNode<T> first;
+    private LinkedListNode<T> last;
+
+    /**
+     * Default constructor.
+     */
+    public MyLinkedList() {
+        size = 0;
+        first = null;
+        last = null;
+    }
+
+    /**
+     * Adds the element to the end of the list.
+     *
+     * @param element the element to add.
+     */
+    public void add(T element) {
+        LinkedListNode<T> node = new LinkedListNode();
+        node.setData(element);
+
+        if (first == null) {
+            first = node;
+        } else if (last == null) {
+            last = node;
+        } else {
+            node.setPrevious(last.getPrevious());
+            node.setNext(last);
+            last.setPrevious(node);
+            first.setNext(node);
+        }
+
+        size++;
+    }
+
+    /**
+     * Inserts the element at the specified index.
+     *
+     * @param index
+     * @param element
+     */
+    public void add(int index, T element) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Cannot insert at index: " + index);
+        }
+
+        LinkedListNode<T> node = new LinkedListNode<>();
+        node.setData(element);
+
+        LinkedListNode<T> prev = null;
+        LinkedListNode<T> next = null;
+
+        if (index == 0) {
+            next = first;
+            first = node;
+        } else if (index == size - 1) {
+            prev = last.getPrevious();
+            next = last;
+        } else {
+            int position = 1;
+
+            prev = first;
+            while (position < index) {
+                prev = prev.getNext();
+                position++;
+            }
+            next = prev.getNext();
+        }
+
+        node.setPrevious(prev);
+        node.setNext(next);
+        prev.setNext(node);
+        next.setPrevious(node);
+
+        size++;
+    }
+
+    /**
+     * Retrieves the element stored at the index position.
+     *
+     * @param index the position in the list of the element.
+     * @return T the element value
+     */
+    public T get(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Cannot insert at index: " + index);
+        }
+
+        if (index == 0) {
+            return first.getData();
+        }
+
+        if (index == size - 1) {
+            return last.getData();
+        }
+
+        int position = 1;
+        LinkedListNode<T> current = first.getNext();
+
+        while (position < index) {
+            current = current.getNext();
+            position++;
+        }
+
+        return current.getData();
+    }
+
+    /**
+     * Removes the element at the given index position.
+     *
+     * @return T the element value.
+     */
+    public T remove(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Cannot insert at index: " + index);
+        }
+
+        T data;
+
+        if (index == 0) {
+            data = first.getData();
+            first.getNext().setPrevious(null);
+            first = first.getNext();
+        } else if (index == size - 1) {
+            data = last.getData();
+            first.getPrevious().setNext(null);
+            last = last.getPrevious();
+        } else {
+            int position = 1;
+
+            LinkedListNode<T> node = first.getNext();
+            while (position < index) {
+                node = node.getNext();
+            }
+            
+            data = node.getData();
+            node.getPrevious().setNext(node.getNext());
+            node.getNext().setPrevious(node.getPrevious());
+        }
+
+        size--;
+        return data;
+    }
+    
+    /**
+     * Retrieves the size of @this instance of LinkedList.
+     * @return 
+     */
+    public int size(){
+        return size;
+    }
+}
